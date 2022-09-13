852c628fffdd4cabfaf9aa8b205c67a1aa1361a3
Jonathan Ramirez <ramirezjj201@gmail.com>
2022-09-13 16:05:21 -0500

    Creación del dao para la consultas a room

---
+++ app/src/main/java/com/jonathan/empowerment/data/datasource/local/dao/RecipeDao.kt
@@ -0,0 +1,17 @@
+package com.jonathan.empowerment.data.datasource.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.jonathan.empowerment.data.datasource.local.entity.RecipeEntity
+
+@Dao
+interface RecipeDao {
+
+    @Query("SELECT * FROM recipes_table ORDER BY id ASC")
+    suspend fun getAllRecipes(): List<RecipeEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllRecipes(recipes: List<RecipeEntity>)
+}
