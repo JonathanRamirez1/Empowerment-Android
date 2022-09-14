c97daedb27de88a35fa8aa9b3dcd44cb1947318b
Jonathan Ramirez <ramirezjj201@gmail.com>
2022-09-14 09:10:33 -0500

    Creación del modulo de room para preparar la database e inyectarla

--- app/src/main/java/com/jonathan/empowerment/utils/Constants.kt
+++ app/src/main/java/com/jonathan/empowerment/utils/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val BASE_URL = "https://api.spoonacular.com/recipes/"
     const val ID_RECIPE: Long = 716429
     const val API_KEY = "faf77d680b1d441a959fc6c8e74fff6d"
+    const val RECIPES_DATABASE_NAME = "recipes_database"
 }
